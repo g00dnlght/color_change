53c4ba9ae0b74b0250a2ead03592f14d4ab55fa0
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-01 18:32:47 -0700

    Updated btn function.
    The button now works as intended. It generates a random number when clicked which in turn changes the colors we chose from the const "colors".

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+// Get all the objects.
+const colors = ['#8675a9', '#c3aed6', "#efbbcf", '#f15025'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function() {
-    // Get random number between 0 - 3
-    const randomNumber = 2;
+
+// Execute function on button click.
+btn.addEventListener('click', function () {
+    // Get random number between 0 - 3.
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+
+// Generate a random number.
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
