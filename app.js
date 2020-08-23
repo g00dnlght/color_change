c7d98318ca018ff5f9a85cdcac477c3c4cc4cc1d
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-22 21:41:32 -0700

    Formatted using prettier
    Formatted using prettier

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 // Get all the objects.
-const colors = ['#867AA9', '#C3C3D6', '#F15025', '#FFFFFF', '#FF0000'];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
-
+const colors = ["#867AA9", "#C3C3D6", "#F15025", "#FFFFFF", "#FF0000"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
 // Execute function on button click.
-btn.addEventListener('click', function () {
-    // Get random number between 0 - 5.
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+btn.addEventListener("click", function () {
+  // Get random number between 0 - 5.
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
-
 
 // Generate a random number.
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * colors.length);
 }
